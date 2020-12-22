a8574c4033a52c6a28919cc262d3c7fff00e6c44
Loïc Pinsard <muream@gmail.com>
2020-12-22 13:53:44 +0100

    add simple Control Class

---
+++ scripts/shapeshifter/control.py
@@ -0,0 +1,27 @@
+import maya.cmds as cmds
+
+from shapeshifter.curve import Curve
+from shapeshifter.utils import get_mobject
+
+
+class Control(object):
+    def __init__(self, curves, transform=None):
+        self.transform = transform
+        self.curves = curves
+
+    @classmethod
+    def from_dict(cls, data):
+        curves = []
+        for curve_data in data.values():
+            print(curve_data)
+            curve = Curve.from_dict(curve_data)
+            curves.append(curve)
+
+        return cls(curves)
+
+    def create(self):
+        if self.transform is None:
+            self.transform = get_mobject(cmds.createNode("transform"))
+
+        for curve in self.curves:
+            curve.create(self.transform)
